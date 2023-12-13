b0e17b4934a22a075c5b2ef2166543b0eb3be547
jiasenwqbr <jasonjiasen@gmail.com>
2023-12-13 11:34:03 +0800

    add nest-status-monitor service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { HelloModule } from './modules/hello/hello.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { ExceptionModule } from './modules/exception/exception.module';
 import { RoleGuardModule } from './modules/role-guard/role-guard.module';
+import { ConfigModule } from 'nestjs-config';
+import { resolve } from 'path';
+import { StatusMonitorModule } from 'nest-status-monitor';
+import statusMonitor from './config/statusMonitor';
 
 @Module({
-  imports: [HelloModule, ExceptionModule, RoleGuardModule],
+  imports: [
+    ConfigModule.load(resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    StatusMonitorModule.setUp(statusMonitor),
+    HelloModule,
+    ExceptionModule,
+    RoleGuardModule,
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
